4c86936716df8fc7cfd9fb3accdb2a6da9e6b7ae
DUONG-PC\Administrator <haduong825>
2018-05-12 10:57:19 -0700

    add something

---
+++ DKTInChi/DKTInChi/Models/Subject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DKTInChi.Models
+{
+    public class Subject
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+        public int numbercredit { get; set; }
+        public int theory { get; set; }
+        public int task { get; set; }
+        public int pratice { get; set; }
+        public int discussion { get; set; }
+        public int totalsession { get; set; }
+    }
+}
