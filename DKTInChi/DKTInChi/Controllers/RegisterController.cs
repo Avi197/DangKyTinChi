c15f49c0307d7635de8b8c5d0e34f3ba3ed978fb
DUONG-PC\Administrator <haduong825>
2018-05-13 15:06:40 +0700

    add register

--- DKTInChi/DKTInChi/Controllers/RegisterController.cs
+++ DKTInChi/DKTInChi/Controllers/RegisterController.cs
@@ -1,13 +1,48 @@
 ﻿using System.Web.Mvc;
+using System.Data.SqlClient;
+using DKTInChi.Models;
+using System.Collections.Generic;
 
 namespace DKTInChi.Controllers
 {
     public class RegisterController : Controller
     {
+
+        public List<Subject> listSubject = new List<Subject>();
+
         // GET
         public ActionResult Index()
         {
+            loadData();
+            ViewData["ListSubject"] = listSubject;
             return View();
+        }
+
+        public ActionResult RegisterAction(string code)
+        {
+            return RedirectToAction("Index");
+        }
+
+        private void loadData()
+        {
+            SqlDataReader reader = CommonFunction.CommonFunction.loadData("SELECT * FROM subjects");
+            while (reader.Read())
+            {
+                string code = (string)reader["code"];
+                string name = (string)reader["name"];
+                int numbercredit = (int)reader["numbercredit"];
+                int theory = (int)reader["theory"];
+                int totalsession = (int)reader["totalsession"];
+                Subject subject = new Subject
+                {
+                    code = code,
+                    name = name,
+                    numbercredit = numbercredit,
+                    theory = theory,
+                    totalsession = totalsession
+                };
+                listSubject.Add(subject);
+            }
         }
     }
 }
